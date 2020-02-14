597379a202d7b93cab9a23ef203d2a3938d32e3b
mansour <smm.2020@gmail.com>
2020-02-14 11:32:41 -0500

    Add project files.

---
+++ Panls/ConnectToDatabase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panls
+{
+    class ConnectToDatabase
+    {
+        VanmeEntities db;
+
+        public ConnectToDatabase()
+        {
+            db = new VanmeEntities();
+
+        }
+        public OwnProduct GiveMeProduct(string str)
+        {
+            var x = db.OwnProducts.Where(p => p.Sku.CompareTo(str) == 0).FirstOrDefault();
+            int yy = int.Parse(str);
+            var y = db.OwnProducts.Where(p => p.Id == yy).FirstOrDefault();
+
+            return y;
+        }
+
+        public List<OwnProduct> GiveListProducts()
+        {
+            var x = db.OwnProducts.ToList();
+            return x;
+        }
+        public List<Company> CompaniesList()
+        {
+            var x = db.Companies.ToList();
+            return x;
+        }
+        public List<Category> CategoriesList()
+        {
+            var x = db.Categories.ToList();
+            foreach (var item in x)
+            {
+                item.Name = item.Name.Trim();
+            }
+            return x;
+        }
+        public List<SubCategory> SubCategoriesList()
+        {
+            var x = db.SubCategories.ToList();
+            foreach (var item in x)
+            {
+                item.Name = item.Name.Trim();
+            }
+            return x;
+        }
+        public List<ProductType> ProductTypeList()
+        {
+            var x = db.ProductTypes.ToList();
+            foreach (var item in x)
+            {
+                item.Name = item.Name.Trim();
+            }
+            return x;
+        }
+    }
+}
---
+++ Panls/SearchProduct.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Panls
+{
+    /// <summary>
+    /// Interaction logic for SearchProduct.xaml
+    /// </summary>
+    public partial class SearchProduct : Window
+    {
+        public SearchProduct()
+        {
+            InitializeComponent();
+
+            ConnectToDatabase connect = new ConnectToDatabase();
+            var t = connect.GiveMeProduct("10006");
+            lblProductTittle.Content ="Product Tittel :  "+ t.ProductTittle;
+            lblCompany.Content ="Company :  "+t.Company.CompanyName;
+            lblCategory.Content ="Category : "+ t.ProductType.CategoriesSubCategory.Category.Name;
+            lblSubCat.Content ="Sub Category :  "+ t.ProductType.CategoriesSubCategory.SubCategory.Name;
+            lblProductType.Content ="Product Type : "+ t.ProductType.Name;
+            lblBrand.Content = "Brand : "+ t.Brand.Name;
+            lblCountryOfOrgin.Content =" Country Of Orgin : "+ t.Country.Name;
+            lblColor.Content = "Color : "+t.Color.Name.Trim();
+            lblMeterial.Content = "Meterial : " + t.Material.MaterialName;
+            lblSku.Content ="SKU : "+ t.Sku;
+            lblStyleNumber.Content = "Style Number :" + t.StyleNumber;
+            lblUPC.Content = "UPC : " + t.Barcode.BarcodeNumber;
+            lblPrice.Content = "Price : " + t.Price.ToString();
+            lblSize.Content= "Size : " + t.Width.ToString() + "*" + t.length.ToString();
+            lblGsm.Content = "GSM : " + t.Gsm.ToString();
+
+        }
+    }
+}
